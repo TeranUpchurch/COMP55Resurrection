97dd7a120980e6ed5337beecb541d47668829f80
zacfe <z_fehner@u.pacific.edu>
2024-11-04 14:57:52 -0800

    created robot class template

---
+++ src/robot.java
@@ -0,0 +1,49 @@
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class robot {
+	private GImage image;
+	private int health;
+	private int damage;
+	private int lane;
+	private int moveSpeed;
+	private int currencyEarned;
+	private Unit unitAttacking;
+	public boolean flag;
+	
+	public robot() {
+		
+	}
+	
+	public int getImageX() {
+		return 0;
+	}
+	
+	public int getImageY() {
+		return 0;
+	}
+	
+	public void damageUnit() {
+		
+	}
+	
+	public void attackUnit() {
+		
+	}
+	
+	public int takeDamage() {
+		return 0;
+	}
+	
+	public boolean isDeath(int health) {
+		return true;
+	}
+	
+	public boolean hasReachUnit() {
+		return true;
+	}
+}
