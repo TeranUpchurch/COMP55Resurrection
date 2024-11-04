544e981ef94efd36337db2a0e62b6687a7241caf
zacfe <z_fehner@u.pacific.edu>
2024-11-04 15:01:15 -0800

    added projectile class template

---
+++ src/projectile.java
@@ -0,0 +1,22 @@
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class projectile {
+	private GImage image;
+	private int width;
+	private int height;
+	private int damage;
+	private int speed;
+	
+	public projectile() {
+		
+	}
+	
+	public void move (int projectileSpeed) {
+		
+	}
+}
