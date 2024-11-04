86cbc503011b5b971f1a3de4918c5443f6b0289a
zacfe <z_fehner@u.pacific.edu>
2024-11-04 15:10:24 -0800

    Added wave class template

---
+++ src/Wave.java
@@ -0,0 +1,26 @@
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class Wave {
+	ArrayList<Robot> robots;
+	ArrayList<Integer> intervals;
+	private int enemyNum;
+	private Timer waveTimer;
+	private int numTimes;
+	
+	public Wave() {
+		
+	}
+	
+	public Robot returnRobot() {
+		return null;
+	}
+	
+	public void onAction (ActionEvent e) {
+		
+	}
+}
