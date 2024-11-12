3eb6e6e0d3be948222a7b35891bbabd11280d1f1
zacfe <z_fehner@u.pacific.edu>
2024-11-12 15:32:01 -0800

    added comments to the robot class to show how the methods will work once implemented

--- src/Robot.java
+++ src/Robot.java
@@ -5,6 +5,7 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import javax.swing.*;
 
+// this class handles the enemy units in the game and their attributes, such as how fast they move and how much health they have
 public class Robot {
 	private GImage image;
 	private int health;
@@ -43,6 +44,7 @@ public class Robot {
 		return 0;
 	}
 	
+	// how much damage the enemy deals to the player unit it is attacking
 	public void damageUnit() {
 		
 	}
@@ -51,14 +53,17 @@ public class Robot {
 		
 	}
 	
+	// how much damage the enemy unit takes from a player unit
 	public int takeDamage() {
 		return 0;
 	}
 	
+	// if an enemy's health reaches zero, it is defeated and disappears from the grid
 	public boolean isDeath(int health) {
 		return true;
 	}
 	
+	// determines if an enemy unit is close enough to a player unit to start attacking and dealing damage to it
 	public boolean hasReachUnit() {
 		return true;
 	}
