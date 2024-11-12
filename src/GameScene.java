98e53d692be3386b57a14393d2e056258befe8a5
Handang04 <maihandang.work@gmail.com>
2024-11-12 00:58:43 -0800

    select unit in unitBar and place it on the screen

--- src/GameScene.java
+++ src/GameScene.java
@@ -14,6 +14,7 @@ public class GameScene extends Scene{
 	private GLabel label;
 	
 	private List<GImage> unitBar = new ArrayList<>();
+	private GImage selectedUnit = null;
 	
 	public static final String IMG_FILENAME_PATH = "media/";
 	public static final String IMG_EXTENSION = ".png";
@@ -25,15 +26,15 @@ public class GameScene extends Scene{
 	}
 	
 	public void drawUnitBar() {
-		String[] itemsImages = {"soldierUnitBar"};
+		String[] unitsImages = {"soldierUnitBar"};
 		double xStart = 20;
 		double yStart = 20;
 		
-		for (int i = 0; i < itemsImages.length; i++) {
-			GImage item = new GImage(IMG_FILENAME_PATH + itemsImages[i] + IMG_EXTENSION);
-			item.setLocation(xStart, yStart);
-			unitBar.add(item); // Add to unitBar list for tracking
-			addElement(item); // Add to display so it appears on screen
+		for (int i = 0; i < unitsImages.length; i++) {
+			GImage unit = new GImage(IMG_FILENAME_PATH + unitsImages[i] + IMG_EXTENSION);
+			unit.setLocation(xStart, yStart);
+			unitBar.add(unit); // Add to unitBar list for tracking
+			addElement(unit); // Add to display so it appears on screen
 		}
 	}
 	
@@ -53,20 +54,43 @@ public class GameScene extends Scene{
 		}
 	}
 	
+	@Override
 	public void mousePressed(MouseEvent e) {
 		System.out.println("Mouse pressed.");
+		// Check if the mouse click is on one of the squares in unit bar
+		for (int i = 0; i < unitBar.size(); i++) {
+			GImage itemUnitBar = unitBar.get(i);
+			if (itemUnitBar.contains(e.getX(), e.getY())) {
+				String[] unit = {"soldier"};
+				selectedUnit = new GImage(IMG_FILENAME_PATH + unit[i] + IMG_EXTENSION);
+				selectedUnit.setLocation(e.getX() - selectedUnit.getWidth() / 2, e.getY() - selectedUnit.getHeight() / 2);
+				addElement(selectedUnit);
+				break;
+			}
+		}
 	}
 	
+	@Override
 	public void mouseReleased(MouseEvent e) {
 		System.out.println("Mouse released.");
+		// When mouse is released, print out the coordinates and placed unit
+		if (selectedUnit != null) {
+			System.out.println("Item placed at x: " + selectedUnit.getX() + "; y: " + selectedUnit.getY());
+			selectedUnit = null;
+		}
 	}
 	
 	public void mouseClicked(MouseEvent e) {
 		System.out.println("Mouse clicked.");
 	}
 	
+	@Override
 	public void mouseDragged(MouseEvent e) {
 		System.out.println("Mouse dragged.");
+		// Move the selected unit with the mouse
+		if (selectedUnit != null) {
+			selectedUnit.setLocation(e.getX() - selectedUnit.getWidth() / 2, e.getY() - selectedUnit.getHeight() / 2);
+		}
 	}
 
 	public static void main(String[] args) {
