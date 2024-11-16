82695e9f4172c92c6aa47cc1eb82df5abcc03578
zacfe <z_fehner@u.pacific.edu>
2024-11-15 16:40:30 -0800

    added constructor for the grid

--- src/Grid.java
+++ src/Grid.java
@@ -3,5 +3,20 @@ import java.util.Vector;
 import java.util.ArrayList;
 
 public class Grid {
-	Unit[][] gameGrid;
+	Space[][] gameGrid;
+	private int rows = 5;
+	private int cols = 9;
+
+	    
+	public Grid(int rows, int cols) {
+		this.rows = rows;
+		this.cols = cols;
+		gameGrid = new Space[rows][cols];
+		
+		for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                gameGrid[row][col] = new Space(row, col);
+            }
+        }
+	}
 }
