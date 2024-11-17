9db48441ecff213ccdb8dba8cadf1c08c2a340a5
Handang04 <maihandang.work@gmail.com>
2024-11-17 00:38:58 -0800

    added upgrade cost

--- src/UnitType.java
+++ src/UnitType.java
@@ -1,18 +1,20 @@
 
 public enum UnitType {
-	SOLDIER ("soldier", "media/soldier.png", 50),
-	MACHINE_GUN ("machineGun", "media/machineGun.png", 100),
-	GRENADE ("grenade", "media/grenade.png", 25),
-	ROCK ("rock", "media/rock.png", 50);
+	SOLDIER ("soldier", "media/soldier.png", 50, 50),
+	MACHINE_GUN ("machineGun", "media/machineGun.png", 100, 100),
+	GRENADE ("grenade", "media/grenade.png", 25, 25),
+	ROCK ("rock", "media/rock.png", 50, 50);
 	
 	private final String name;
 	private final String imagePath;
 	private final int cost;
+	private final int upgradeCost;
 	
-	UnitType(String name, String imagePath, int cost) {
+	UnitType(String name, String imagePath, int cost, int upgradeCost) {
 		this.name = name;
 		this.imagePath = imagePath;
 		this.cost = cost;
+		this.upgradeCost = upgradeCost;
 	}
 	
 	public String getName() {
@@ -25,5 +27,9 @@ public enum UnitType {
 	
 	public int getCost() {
 		return this.cost;
+	}
+	
+	public int getUpgradeCost() {
+		return this.upgradeCost;
 	}
 }
