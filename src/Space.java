020f2c03af47dffb444eb3b47a7683f993869b52
zacfe <z_fehner@u.pacific.edu>
2024-11-17 18:53:23 -0800

    updated the space class

--- src/Space.java
+++ src/Space.java
@@ -1,25 +1,38 @@
-public class Space {
- 
-	private int row;
-	private int col;
+import acm.graphics.GImage;
 
-	public Space(int row, int col) {
-		this.row = row;
-		this.col = col;
-	}
-	
-	public int getRow() {
-		return row;
-	}
-	public void setRow(int row) {
-		this.row = row;
-	}
-	
-	public int getCol() {
-		return col;
-	}
-	
-	public void setCol(int col) {
-		this.col = col;
-	}
+public class Space {
+    private int row;
+    private int col;
+    private Unit unit; // The unit in this space, if any
+
+    public Space(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.unit = null;
+    }
+
+    public void placeUnit(Unit unit, int x, int y) {
+        this.unit = unit;
+        unit.setImagePos(x, y); // Sets a unit's graphical position
+    }
+
+    public void removeUnit() {
+        if (unit != null) {
+            unit.getImageFromUnit().setVisible(false); // Removes a graphical image
+            this.unit = null;
+        }
+    }
+
+    public boolean isOccupied() {
+        return unit != null;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    @Override
+    public String toString() {
+        return (unit != null) ? "U" : " ";  // Simplified
+    }
 }
