1baf7d979b2607f1c9b2e3a3246fbb05d6e57944
Kris Chau <k_chau9@u.pacific.edu>
2024-12-07 23:14:49 -0800

    Configured the difficulties further

--- src/Level.java
+++ src/Level.java
@@ -51,11 +51,11 @@ public class Level {
 			waves.add(new Wave(gameScene, robots1, intervals1));
 			
 			ArrayList<Robot> robots2 = new ArrayList<Robot>();
-			robots2.add(new RobotGrey(0, gameScene));
-			robots2.add(new RobotGrey(1, gameScene));
+			robots2.add(new RobotGreen(0, gameScene));
+			robots2.add(new RobotGreen(1, gameScene));
 			robots2.add(new RobotGrey(2, gameScene));
 			robots2.add(new RobotYellow(3, gameScene));
-			robots2.add(new RobotGrey(2, gameScene));
+			robots2.add(new RobotGreen(2, gameScene));
 			ArrayList<Integer> intervals2 = new ArrayList<Integer>();
 			intervals2.add(50);
 			intervals2.add(50);
@@ -84,22 +84,20 @@ public class Level {
 			robots1.add(new RobotGreen(1, gameScene));
 			robots1.add(new RobotGreen(2, gameScene));
 			robots1.add(new RobotGreen(3, gameScene));
-			robots1.add(new RobotYellow(2, gameScene));
-			robots1.add(new RobotGrey(4, gameScene));
+			robots1.add(new RobotGreen(2, gameScene));
 			ArrayList<Integer> intervals1 = new ArrayList<Integer>();
 			intervals1.add(50);
 			intervals1.add(50);
 			intervals1.add(50);
 			intervals1.add(50);
-			intervals1.add(1);
 			waves.add(new Wave(gameScene, robots1, intervals1));
 			
 			ArrayList<Robot> robots2 = new ArrayList<Robot>();
 			robots2.add(new RobotGrey(0, gameScene));
 			robots2.add(new RobotYellow(1, gameScene));
-			robots2.add(new RobotYellow(2, gameScene));
+			robots2.add(new RobotGreen(2, gameScene));
 			robots2.add(new RobotYellow(3, gameScene));
-			robots2.add(new RobotYellow(2, gameScene));
+			robots2.add(new RobotGreen(2, gameScene));
 			ArrayList<Integer> intervals2 = new ArrayList<Integer>();
 			intervals2.add(10);
 			intervals2.add(10);
@@ -107,6 +105,20 @@ public class Level {
 			intervals2.add(10);
 			intervals2.add(10);
 			waves.add(new Wave(gameScene, robots2, intervals2));
+			
+			ArrayList<Robot> robots3 = new ArrayList<Robot>();
+			robots3.add(new RobotGrey(0, gameScene));
+			robots3.add(new RobotGreen(1, gameScene));
+			robots3.add(new RobotYellow(2, gameScene));
+			robots3.add(new RobotGreen(3, gameScene));
+			robots3.add(new RobotGrey(4, gameScene));
+			ArrayList<Integer> intervals3 = new ArrayList<Integer>();
+			intervals3.add(10);
+			intervals3.add(10);
+			intervals3.add(10);
+			intervals3.add(10);
+			intervals3.add(10);
+			waves.add(new Wave(gameScene, robots3, intervals3));
 		}
 		else if (difficulty == "Hard")
 		{
