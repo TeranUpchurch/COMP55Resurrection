e42b25a4ca71350c9484b28ccd1158e231d6c39a
Handang04 <maihandang.work@gmail.com>
2024-12-08 17:13:43 -0800

    configured the difficulties

--- src/Level.java
+++ src/Level.java
@@ -123,27 +123,44 @@ public class Level {
 		else if (difficulty == "Hard")
 		{
 			ArrayList<Robot> robots1 = new ArrayList<Robot>();
-			robots1.add(new RobotGreen(1, gameScene));
+			robots1.add(new RobotGreen(0, gameScene));
 			robots1.add(new RobotGreen(2, gameScene));
 			robots1.add(new RobotGreen(3, gameScene));
+			robots1.add(new RobotGreen(0, gameScene));
+			robots1.add(new RobotGreen(2, gameScene));
+			robots1.add(new RobotGreen(3, gameScene));
+			robots1.add(new RobotGreen(2, gameScene));
 			robots1.add(new RobotYellow(2, gameScene));
 			robots1.add(new RobotGrey(4, gameScene));
 			ArrayList<Integer> intervals1 = new ArrayList<Integer>();
 			intervals1.add(50);
 			intervals1.add(50);
 			intervals1.add(50);
+			intervals1.add(30);
+			intervals1.add(30);
+			intervals1.add(30);
+			intervals1.add(50);
 			intervals1.add(50);
 			intervals1.add(1);
 			waves.add(new Wave(gameScene, robots1, intervals1));
 			
 			ArrayList<Robot> robots2 = new ArrayList<Robot>();
+			robots2.add(new RobotGreen(0, gameScene));
+			robots2.add(new RobotGreen(2, gameScene));
+			robots2.add(new RobotGreen(3, gameScene));
 			robots2.add(new RobotGrey(0, gameScene));
+			robots2.add(new RobotGrey(2, gameScene));
+			robots2.add(new RobotGrey(2, gameScene));
 			robots2.add(new RobotYellow(1, gameScene));
 			robots2.add(new RobotYellow(2, gameScene));
 			robots2.add(new RobotYellow(3, gameScene));
 			robots2.add(new RobotYellow(2, gameScene));
 			ArrayList<Integer> intervals2 = new ArrayList<Integer>();
 			intervals2.add(10);
+			intervals2.add(10);
+			intervals2.add(10);
+			intervals2.add(10);
+			intervals2.add(30);
 			intervals2.add(10);
 			intervals2.add(10);
 			intervals2.add(10);
